8416869f0cf92120664a7f246458090a95e08616
YOUR NAME <blzhang.m@gmail.com>
2015-12-29 21:42:29 -0500

    POJ2686 DP compressed

--- poj/POJ2686.cpp
+++ poj/POJ2686.cpp
@@ -1,65 +1,64 @@
+/*This is the solution for POJ2686
+ * the method used here is DP
+ */
+
 #include<iostream>
 #include<vector>
 #include<stdio.h>
+#include<cmath>
 using namespace std;
 
 const int max_n = 8;
-const int max_m = 10;
-const double INF = 1 << 20;
+const int max_m = 30;
+const double INF = 1000000000000.0;
 int n, m,p, a, b;
 
-vector<int> t(max_n);
-vector<vector<int>  > d(max_m);
-vector<vector<double> > dp( 1 << max_m);
-
+int t[max_n];
+int d[max_m][max_m];
+double dp[1 << max_n][max_m];
 int main(){
-	for(int i = 0 ; i < max_m; i++){
-		d[i].resize(max_m);
-	}
-	for(int i = 0 ; i < 1 << max_m; i++){
-		dp[i].resize(max_m);
-	}
-	for(int i = 0 ; i < max_m ; i++){
-		for(int j = 0 ;j  < max_m ;j ++){
-			d[i][j] = -1;
-		}
-	}
 	freopen("in.txt","r",stdin);
 	while(scanf("%d %d %d %d %d", &n, &m, &p, &a, &b)){
 		if(n == 0 && m == 0 && p == 0 && a == 0 && b == 0) break;
+		for(int i = 0 ; i < max_m ; i++){
+			for(int j = 0 ;j  < max_m ;j ++){
+				d[i][j] = -1;
+			}
+		}
 		for(int i = 0 ; i < n ; i++){
 			scanf("%d", &t[i]);
 		}
 		int  _i, _j;
 		for(int i = 0 ; i < p; i++){
 			scanf("%d %d", &_i, &_j);
-			scanf("%d", &d[_i][_j]);
+			scanf("%d", &d[_i-1][_j-1]);
+			d[_j-1][_i-1] = d[_i-1][_j-1];
 		}
-		for( int i = 0 ; i < n ; i++){
+		for( int i = 0 ; i < 1 << n ; i++){
 			for(int j = 0 ; j < m; j++){
 				dp[i][j] = INF;
 			}
 		}
-		dp[(1 << n) - 1][a - 1] = 0;
+		dp[(1 << n) - 1][a - 1] = 0.0;
 		double res = INF;
 		for(int S = ( 1 << n ) -1; S >= 0; S--){
 			res = min(res, dp[S][b-1]);
 			for( int v = 0 ; v  < m ; v++){
 				for(int i = 0 ; i < n ; i++){
-					if ( S>> i &1){
+					if ( S >> i & 1){
 						for ( int u = 0; u < m; u++){
 							if(d[v][u] >= 0){
-								dp[S & ~(1 << i)][u] = min(dp[S&~(1 << i)][u], dp[S][u] + (double)d[v][u]/t[i]);
+								dp[S & ~(1 << i)][u] = min(dp[S&~(1 << i)][u], dp[S][v] + double(d[v][u])/t[i]);
 							}
 						}
 					}
 				}
 			}
 		}
-		if(res == INF){
+		if(abs(res - INF) < 0.00001){
 			printf("Impossible\n");
 		}else{
-			printf("%lf\n", res);
+			printf("%.3f\n", res); // %lf will be wrong
 		}
 	}
 	fclose(stdin);
